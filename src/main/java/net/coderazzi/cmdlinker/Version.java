5e88d595266620dfd9395731b8339b5532335020
coderazzi <coderazzi@gmail.com>
2025-01-06 03:47:39 +0100

    Corrected version as 0.98

--- src/main/java/net/coderazzi/cmdlinker/Version.java
+++ src/main/java/net/coderazzi/cmdlinker/Version.java
@@ -2,6 +2,6 @@ package net.coderazzi.cmdlinker;
 
 public abstract class Version {
     public static String getVersion() {
-        return "CmdLinker v0.9.8\n(c) Luis M Pena, July 2007\nhttps://coderazzi.net/cmdlinker";
+        return "CmdLinker v0.98\n(c) Luis M Pena, July 2007\nhttps://coderazzi.net/cmdlinker";
     }
 }
